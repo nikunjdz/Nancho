d94613b0188a8cb0980cd4d8ed340853a28a2bca
nikunjdz <tanwarnikunj10@gmail.com>
2026-05-31 14:32:42 +0530

    chore: update sidebars with correct project URLs

--- src/components/LeftSidebar.tsx
+++ src/components/LeftSidebar.tsx
@@ -11,9 +11,9 @@ import mangetImg from "@/assets/images/mangets7.gif";
 const links = [
   { image: sapLogoImg, label: "Sacred Animals", sublabel: "Party Site", href: "#" },
   { image: m10LogoImg, label: "Magic 10% Site", sublabel: "", href: "#" },
-  { image: face2Img, label: "Excise BIG Personhood,", sublabel: "Neuter Corporate Rulers", href: "#" },
-  { image: achilletImg, label: "Reinscribe Corporate Codes,", sublabel: "Re-engineer Big DNA", href: "#" },
-  { image: snake3Gif, label: "Remasculate Democracy,", sublabel: "The Big T and Sovereignty", href: "#" },
+  { image: face2Img, label: "Excise BIG Personhood,", sublabel: "Neuter Corporate Rulers", href: "https://nikunjdz.github.io/excise-big/" },
+  { image: achilletImg, label: "Reinscribe Corporate Codes,", sublabel: "Re-engineer Big DNA", href: "https://nikunjdz.github.io/reinscrive/" },
+  { image: snake3Gif, label: "Remasculate Democracy,", sublabel: "The Big T and Sovereignty", href: "https://nikunjdz.github.io/remasculate/" },
   { image: bigBodthImg, label: "Practice Big Med at Home!", sublabel: "The Big Medicine Mailing List", href: "#" },
 ];
 
@@ -21,7 +21,7 @@ const LeftSidebar = () => {
   return (
     // Increased width from 132px to 180px
     <div className="w-[180px] flex flex-col items-center gap-10 p-4 text-center bg-transparent min-h-screen flex-shrink-0 border-r border-gray-800">
-      
+
       {/* Header Section */}
       <div className="flex flex-col items-center mb-6">
         {/* Increased logo size from w-14 to w-20 */}
@@ -34,7 +34,12 @@ const LeftSidebar = () => {
 
       <nav className="flex flex-col items-center space-y-10">
         {links.map((link, i) => (
-          <a key={i} href={link.href} className="flex flex-col items-center group no-underline">
+          <a
+            key={i}
+            href={link.href}
+            className="flex flex-col items-center group no-underline"
+            {...(link.href !== "#" ? { target: "_blank", rel: "noopener noreferrer" } : {})}
+          >
             {/* Increased image size from w-16 to w-28 */}
             <img
               src={link.image}
--- src/components/RightSidebar.tsx
+++ src/components/RightSidebar.tsx
@@ -10,20 +10,20 @@ import fdlapImg from "@/assets/images/FdlogoS.gif";
 
 const archives = [
   { image: cop10Img, label: "COP10 Conference", sublabel: "Archive", href: "#" },
-  { image: truthImg, label: "Truth Emergency", sublabel: "Archive", href: "#" },
-  { image: wbaiImg, label: "Ain't that Good News", sublabel: "WBAI Radio Show Archive", href: "#" },
-  { image: chautauquaImg, label: "New Chautauqua", sublabel: "Archive", href: "#" },
-  { image: karmaImg, label: "Index for Original", sublabel: "Big Karma Site", href: "#" },
-  { image: vprezImg, label: "Big Med 2000", sublabel: "V-Prez Campaign", href: "#" },
-  { image: memeImg, label: "Meme Works", sublabel: "", href: "#" },
-  { image: fdlapImg, label: "Forum of Democratic Leaders", sublabel: "of the Asia-Pacific", href: "#" },
+  { image: truthImg, label: "Truth Emergency", sublabel: "Archive", href: "https://nikunjdz.github.io/truthemergency/" },
+  { image: wbaiImg, label: "Ain't that Good News", sublabel: "WBAI Radio Show Archive", href: "https://nikunjdz.github.io/Aintthatgood/" },
+  { image: chautauquaImg, label: "New Chautauqua", sublabel: "Archive", href: "https://nikunjdz.github.io/newchau-/newchau/arc/newindex.html" },
+  { image: karmaImg, label: "Index for Original", sublabel: "Big Karma Site", href: "https://nikunjdz.github.io/indexfororg/" },
+  { image: vprezImg, label: "Big Med 2000", sublabel: "V-Prez Campaign", href: "https://nikunjdz.github.io/bigmed2000/" },
+  { image: memeImg, label: "Meme Works", sublabel: "", href: "https://nikunjdz.github.io/memework/" },
+  { image: fdlapImg, label: "Forum of Democratic Leaders", sublabel: "of the Asia-Pacific", href: "https://nikunjdz.github.io/formere/" },
 ];
 
 const RightSidebar = () => {
   return (
     // Increased width from 132px to 180px and padded the container
     <div className="w-[180px] flex flex-col items-center gap-10 p-4 text-center bg-transparent min-h-screen flex-shrink-0 border-l border-gray-800">
-      
+
       {/* Header Section */}
       <div className="flex flex-col items-center mb-6">
         {/* Increased logo size from w-14 to w-20 */}
@@ -37,7 +37,12 @@ const RightSidebar = () => {
       {/* Navigation Section */}
       <nav className="flex flex-col items-center space-y-10">
         {archives.map((link, i) => (
-          <a key={i} href={link.href} className="flex flex-col items-center group no-underline">
+          <a
+            key={i}
+            href={link.href}
+            className="flex flex-col items-center group no-underline"
+            {...(link.href !== "#" ? { target: "_blank", rel: "noopener noreferrer" } : {})}
+          >
             {/* Increased image size from w-16 to w-28 */}
             <img
               src={link.image}
